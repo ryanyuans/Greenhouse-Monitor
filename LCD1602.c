83613889f35421f86384219c565aa43d7fa9c3b8
ryanyuans <ryan.yuanss@gmail.com>
2015-10-03 16:30:29 +0800

    温室大棚监控系统
    Version 0

---
+++ LCD1602.c
@@ -0,0 +1,113 @@
+ /* 
+ *****************************************
+			1602 液晶驱动模块
+ *****************************************
+ */
+
+#include<reg52.h>
+
+#define LCD_DB P0
+
+#define uchar unsigned char
+#define uint unsigned int
+#define ulong unsigned long
+	
+sbit LCD_RS = P1^0;
+sbit LCD_RW = P1^1;
+sbit LCD_E = P1^5;
+
+void Reset1602()		// 重新配置LCD1602 向LCD1602写入命令
+{
+	void WriteCmd(uchar cmd);
+	
+	WriteCmd(0X38);
+	WriteCmd(0X0C);
+	WriteCmd(0X06);
+	WriteCmd(0X01);
+}
+
+void WriteCmd(uchar cmd)	 	// 写入命令 配置 ———— 底层
+{
+	void WaitReady();
+	
+	WaitReady();
+	LCD_RS = 0;
+	LCD_RW = 0;
+	LCD_DB = cmd;
+	LCD_E = 1;
+	LCD_E = 0;
+
+}
+
+void WaitReady()		// 	判断“忙”，并决策 “等待” 或 “执行”    change something.		
+{
+	uchar str;
+	
+	LCD_DB = 0XFF;
+	LCD_RS = 0;
+	LCD_RW = 1;
+	
+	do
+	{
+		LCD_E = 1;
+		str = LCD_DB;
+		LCD_E = 0;
+	}while(str & 0x80);
+	
+}
+
+void ShowStr(uchar x,uchar y,uchar *str)		// 传地址 + 传数据
+{
+	void SetLoc(uchar x,uchar y);
+	void WriteData(uchar dat);
+	
+	SetLoc(x,y);
+	
+	while(*str != '\0')
+	{
+			WriteData(*str++);
+	}
+}
+
+void SetLoc(uchar x,uchar y)		// 写入的【数据】地址 配置 ———— 底层
+{
+	uchar addr;
+	
+	if(y == 0)
+	{
+			addr = 0x80 + 0x00 + x;
+	}
+	else
+	{
+			addr = 0x80 + 0x40 + x;
+	}
+	WriteCmd(addr);
+}
+
+void WriteData(uchar dat)		// 向 LCD1602 上写入数据 配置 ———— 底层
+{
+	
+	WaitReady();
+	
+	LCD_RS = 1;
+	LCD_RW = 0;
+	LCD_DB = dat;
+	LCD_E = 1;
+	LCD_E = 0;
+}
+/*
+void ClearAll()
+{
+	WriteCmd(0X01);
+}
+
+void ClearArea(uchar x,uchar y,uchar len)		// 指定位置清屏 Area
+{
+	SetLoc(x,y);
+	
+	while(len--)
+	{
+			WriteData(' ');
+	}
+}
+*/
---
+++ Main.c
@@ -0,0 +1,387 @@
+//****************************************************************//
+//	   DHT21使用范例 
+//单片机 AT89S52 或 STC89C52RC 
+//			Ryan_yuans
+//****************************************************************//
+
+#include <reg51.h>
+#include <intrins.h> 
+//
+typedef unsigned char  U8;       /* defined for unsigned 8-bits integer variable 	  无符号8位整型变量  */
+typedef signed   char  S8;       /* defined for signed 8-bits integer variable		  有符号8位整型变量  */
+typedef unsigned int   U16;      /* defined for unsigned 16-bits integer variable 	  无符号16位整型变量 */
+typedef signed   int   S16;      /* defined for signed 16-bits integer variable 	  有符号16位整型变量 */
+typedef unsigned long  U32;      /* defined for unsigned 32-bits integer variable 	  无符号32位整型变量 */
+typedef signed   long  S32;      /* defined for signed 32-bits integer variable 	  有符号32位整型变量 */
+typedef float          F32;      /* single precision floating point variable (32bits) 单精度浮点数（32位长度） */
+typedef double         F64;      /* double precision floating point variable (64bits) 双精度浮点数（64位长度） */
+//
+#define uchar unsigned char
+#define uint unsigned int
+#define   Data_0_time    4
+
+extern void ShowStr(uchar x,uchar y,uchar *str);
+extern void Reset1602();
+
+//----------------------------------------------//
+//----------------IO口定义区--------------------//
+//----------------------------------------------//
+sbit P2_0  = P2^0 ;		// 温湿度数据
+sbit DOUT=P2^1;			// 有害气体浓度数据
+
+sbit BUZZ = P1^6;
+
+sbit INA = P1^0;
+sbit INB = P1^1;
+sbit ENA = P1^2;
+
+//----------------------------------------------//
+//----------------定义区--------------------//
+//----------------------------------------------//
+U8  U8FLAG;
+U8  U8count,U8temp;
+U8  U8T_data_H = 0, U8T_data_L = 0, U8RH_data_H = 0, U8RH_data_L = 0, U8checkdata = 0;
+U8  U8T_data_H_temp = 0, U8T_data_L_temp = 0, U8RH_data_H_temp = 0, U8RH_data_L_temp = 0, U8checkdata_temp = 0;
+U8  U8comdata;
+U8  count, count_r=0;
+U16 U16temp1,U16temp2;
+U8 T0RH = 0;
+U8 T0RL = 0;
+U8 T1RH = 0;
+U8 T1RL = 0;
+U16 flag1s = 0;
+uchar str[10];
+U8 enbuzz1 = 0,enbuzz2 = 0,enbuzz3 = 0;
+U8 ENA_Temp1 = 0,ENA_Temp2 = 0;
+
+/*
+void SendData(U8 *a)
+{
+    outdata[0] = a[0]; 
+	outdata[1] = a[1];
+	outdata[2] = a[2];
+	outdata[3] = a[3];
+	outdata[4] = a[4];
+	count = 1;
+	SBUF=outdata[0];
+}
+*/
+
+void TrasfToString(U8 *str,U8 dat)
+{
+	signed char i = 0;
+	uchar len = 0;
+	uchar buf[6];
+	
+	if(dat < 0)
+	{
+		dat = -dat;
+		*str++ = '-';
+		len++;
+	}
+	
+	do
+	{
+		buf[i++] = dat % 10;
+		dat /= 10;
+	}while(dat > 0);
+	
+	len += i;
+	
+	while(i-- > 0)
+	{
+		*str++ = buf[i] + '0';
+	}
+	
+	*str = '\0';
+
+}
+
+
+void Delay(U16 j)
+{      U8 i;
+	for(;j>0;j--)
+	{ 	
+	for(i=0;i<27;i++);
+
+	}
+}
+
+void  Delay_10us(void)
+{
+	U8 i;
+	i--;
+	i--;
+	i--;
+	i--;
+	i--;
+	i--;
+}
+	
+void  COM(void)
+ {
+
+	U8 i;
+	  
+	for(i=0;i<8;i++)	   
+	{
+	
+		U8FLAG=2;	
+	while((!P2_0)&&U8FLAG++)
+		;
+	Delay_10us();
+	Delay_10us();
+	Delay_10us();
+	U8temp=0;
+		
+	 if(P2_0)
+		 U8temp=1;
+	 
+	 U8FLAG=2;
+	 
+	 while((P2_0)&&U8FLAG++)
+		 ;
+	//超时则跳出for循环		  
+	 if(U8FLAG==1)
+		 break;
+	//判断数据位是0还是1	 
+	   
+	// 如果高电平高过预定0高电平值则数据位为 1 
+	 
+	   U8comdata <<= 1;
+	   U8comdata |= U8temp;        // 0 or 1
+	 }//rof
+   
+}
+
+	/*
+	//--------------------------------
+	//-----湿度读取子程序 ------------
+	//--------------------------------
+	//----以下变量均为全局变量--------
+	//----温度高8位== U8T_data_H------
+	//----温度低8位== U8T_data_L------
+	//----湿度高8位== U8RH_data_H-----
+	//----湿度低8位== U8RH_data_L-----
+	//----校验 8位 == U8checkdata-----
+	//----调用相关子程序如下----------
+	//---- Delay();, Delay_10us();,COM(); 
+	//-------------------------------- ↓
+	*/
+void RH(void)
+{
+
+  //主机拉低18ms 
+   P2_0=0;
+   Delay(180);
+   P2_0=1;
+ //总线由上拉电阻拉高 主机延时20us
+   Delay_10us();
+   Delay_10us();
+   Delay_10us();
+   Delay_10us();
+ //主机设为输入 判断从机响应信号 
+   P2_0=1;
+ //判断从机是否有低电平响应信号 如不响应则跳出，响应则向下运行	  
+   if(!P2_0)		 //T !	  
+   {
+	   	TR0 = 0;	//停止定时器计时, 防止中断干扰温湿度数据采集
+		TR1 = 0;
+	
+	   U8FLAG=2;
+	 //判断从机是否发出 80us 的低电平响应信号是否结束	 
+	   while((!P2_0)&&U8FLAG++);
+	   U8FLAG=2;
+	 //判断从机是否发出 80us 的高电平，如发出则进入数据接收状态
+	   while((P2_0)&&U8FLAG++);
+	 //数据接收状态		 
+	   COM();
+	   U8RH_data_H_temp=U8comdata;
+	   COM();
+	   U8RH_data_L_temp=U8comdata;
+	   COM();
+	   U8T_data_H_temp=U8comdata;
+	   COM();
+	   U8T_data_L_temp=U8comdata;
+	   COM();
+	   U8checkdata_temp=U8comdata;
+	   P2_0=1;
+		   
+	 //数据校验 ↓
+			
+	   U8temp = (U8T_data_H_temp + U8T_data_L_temp + U8RH_data_H_temp + U8RH_data_L_temp);
+	   if(U8temp == U8checkdata_temp)
+	   {
+		  U8RH_data_H = U8RH_data_H_temp;
+		  U8RH_data_L = U8RH_data_L_temp;
+		  U8T_data_H = U8T_data_H_temp;
+		  U8T_data_L = U8T_data_L_temp;
+		  U8checkdata = U8checkdata_temp;
+
+	   }//fi
+   }//fi
+   
+	TR0 = 1;	// 重新打开定时器计时
+	TR1 = 1;
+}
+	
+void ConfigTimer0(U16 ms)	// 设置定时器T0 底层驱动
+{
+	U32 tmp;
+	
+	tmp = 11059200 / 12;
+	tmp = (tmp * ms) / 1000;
+	tmp = 65536 - tmp;
+	tmp = tmp + 13;
+	T0RH = (uchar)(tmp >> 8);
+	T0RL = (uchar)tmp;
+	TH0 = T0RH;
+	TL0 = T0RL;
+	TMOD &= 0XF0;
+	TMOD |= 0X01;
+	ET0 = 1;
+	TR0 = 1;
+}
+
+void ConfigTimer1(U16 ms)	// 设置定时器T1 底层驱动
+{
+	U32 tmp;
+	
+	tmp = 11059200 / 12;
+	tmp = (tmp * ms) / 1000;
+	tmp = 65536 - tmp;
+	tmp = tmp + 2;
+	T1RH = (uchar)(tmp >> 8);
+	T1RL = (uchar)tmp;
+	TH1 = T0RH;
+	TL1 = T0RL;
+	TMOD &= 0X0F;
+	TMOD |= 0X10;
+	ET1 = 1;
+	TR1 = 1;
+}
+	
+//----------------------------------------------
+void main()
+{
+	
+	INA = 0;
+	INB = 1;
+	ENA = 0;
+	
+	EA = 1;
+	ConfigTimer0(20);		// 20 X 50 = 1 S
+	ConfigTimer1(1);	// ~ BUZZ
+	Reset1602();
+	
+	ShowStr(0,0,"Temp:");
+	ShowStr(0,1,"Humi:");
+	
+	Delay(1);         //延时100US（12M晶振)
+	
+	while(1)
+	{  
+		
+		if(DOUT==0)		// 读取有害气体浓度是否超标
+			{
+				Delay(1);	// 延时再次确认
+				if(DOUT == 0)
+				{
+					enbuzz1 = 1;
+					ENA_Temp1 = 1;
+				}
+
+			}
+		 else
+			{
+				enbuzz1 = 0;
+				ENA_Temp1 = 0;
+			}
+	
+
+	   //------------------------
+	   //调用温湿度读取子程序 读取温湿度！
+		RH();
+			
+			
+		if(flag1s == 1)		// 将温湿度显示在液晶屏上
+		{
+			flag1s = 0;
+			 TrasfToString(str,U8T_data_H);
+			ShowStr(6,0,str);
+			TrasfToString(str,U8RH_data_H);
+			ShowStr(6,1,str);
+			
+			if(U8T_data_H >= 35)	// 当温度 >= 35°C 时
+			{
+				ShowStr(14,0,"!~");
+				ENA_Temp2 = 1;
+				enbuzz2 = 1;
+			}
+			else
+			{
+				ShowStr(14,0,"  ");
+				ENA_Temp2 = 0;
+				enbuzz2 = 0;
+			}
+			
+			if(U8RH_data_H >= 50)	// 当湿度 >= 50% 时
+			{
+				ShowStr(14,1,"!~");
+				enbuzz3 = 1;
+			}
+			else
+			{
+				ShowStr(14,1,"  ");
+				enbuzz3 = 0;
+			}
+			
+		}	
+		
+		if((ENA_Temp1 == 1) || (ENA_Temp2 == 1))	// 启动风扇 抽风 降温
+		{
+			ENA = 1;
+		}
+		else
+		{
+			ENA = 0;
+		}
+		
+		
+	}//while
+	
+}// main
+
+
+void Timer0() interrupt 1
+{
+	static U8 timer = 0;
+	
+	TH0 = T0RH;
+	TL0 = T0RL;
+	
+	
+	timer++;
+	
+	if(timer == 50)		// 计时 20 X 50 ==> 1 S
+	{
+		timer = 0;
+		flag1s = 1;
+	}
+}
+
+void Timer1() interrupt 3
+{
+	TH1 = T1RH;
+	TL1 = T1RL;
+	
+	if((enbuzz1 == 1) || (enbuzz2 == 1) || ((enbuzz3 == 1)))	// 打开蜂鸣器 ~BUZZ
+	{
+		BUZZ = ~BUZZ;
+	}
+	else
+	{
+		BUZZ = 1;
+	}
+	
+}
